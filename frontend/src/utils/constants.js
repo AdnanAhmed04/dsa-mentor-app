7896d209c17318f4bae31a6f437a8e942e6ae89c
AdnanAhmed04 <adnanahmedb7208@gmail.com>
2026-05-19 17:25:22 +0500

    Update API URL to Vercel production backend

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,7 +1,1 @@
-import { Platform } from 'react-native';
-
-// Use your computer's LAN IP address here if running on a physical device
-// You can find it by running 'ipconfig' (Windows) or 'ifconfig' (Mac/Linux)
-export const API_URL = Platform.OS === 'android' 
-  ? 'http://192.168.100.30:5000/api' 
-  : 'http://192.168.100.30:5000/api';
+export const API_URL = 'https://dsa-mentor-2pawn7lmx-adnans-projects-55c25dfb.vercel.app/api';
